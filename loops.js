0bc8570ba9fb919dea869c9838a143684ee04edb
SSI90 <ss.ishaak@gmail.com>
2023-10-21 22:25:37 +0200

    loops opdrachten gemaakt

--- loops.js
+++ loops.js
@@ -7,12 +7,18 @@
 // ****
 // *****
 
+for (let i = 0; i < 1; i++) {
+    console.log("*".repeat(1));
+    console.log("*".repeat(2));
+    console.log("*".repeat(3));
+    console.log("*".repeat(4));
+    console.log("*".repeat(5));
+
+}
+
 // Tip: je kunt de .repeat() methode gebruiken om een karakter een n aantal keer te herhalen...
 // Dit heb je nog niet geleerd, maar bekijk hiervoor dit MDN-artikel eens: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat
 // ==========================================
-
-
-
 
 
 // ==========================================
@@ -24,9 +30,13 @@
 // loop...
 // klaar!
 // ==========================================
+for (let i = 0; i < 4; i++) {
+    console.log("loop...");
+}
 
-
-
+for (let i = 0; i < 1; i++){
+    console.log("klaar!");
+}
 
 
 // ==========================================
@@ -45,9 +55,17 @@
 // >> 9
 // ==========================================
 
+for (let i = 0; i < 10; i++){
 
-
-
+    if (i > 2 === i < 6) {
+        console.log("> " + i);
+    } else if (i > 5)
+    {
+        console.log(">> " + i);
+    } else {
+        console.log(i)
+    }
+}
 
 // ==========================================
 // Opdracht 4 (BONUS): schrijf een for-loop die van 0 tot 100 loopt en de getallen print.
